bf254b05f5025074dea57ec58eaea77fc8f42a4a
Maximillian Arruda <dearrudam@gmail.com>
2025-07-12 12:18:46 -0300

    feat: implement asynchronous payment request handling
    Introduced a queue for managing payment requests asynchronously,
    improving the processing efficiency and responsiveness of the
    PaymentProcessorServiceExecutor.
    
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- code-with-quarkus/src/main/java/org/acme/application/PaymentProcessorServiceExecutor.java
+++ code-with-quarkus/src/main/java/org/acme/application/PaymentProcessorServiceExecutor.java
@@ -11,12 +11,17 @@ import org.acme.health.PaymentProcessorHealthState;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.stream.IntStream;
+
+import static java.util.Optional.ofNullable;
 
 @ApplicationScoped
 public class PaymentProcessorServiceExecutor {
 
+    private final LinkedBlockingDeque<NewPaymentRequest> queue = new LinkedBlockingDeque<>();
     private ExecutorService executorService;
     private final Map<RemotePaymentName, PaymentProcessorHealthState> healthState;
     private final Payments payments;
@@ -32,6 +37,22 @@ public class PaymentProcessorServiceExecutor {
     @PostConstruct
     public void postConstruct() {
         this.executorService = Executors.newVirtualThreadPerTaskExecutor();
+        IntStream.range(0, Runtime.getRuntime().availableProcessors())
+                .forEach(_ -> {
+                    this.executorService.submit(() -> {
+                        while (true) {
+                            NewPaymentRequest request = null;
+                            try {
+                                request = queue.take();
+                                ofNullable(request).ifPresent(new NewPaymentItemExecutor(healthState, payments)::accept);
+                            } catch (InterruptedException e) {
+                                // I don't care about it
+                            } catch (RuntimeException e) {
+                                queue.offer(request);
+                            }
+                        }
+                    });
+                });
     }
 
     @PreDestroy
@@ -40,7 +61,8 @@ public class PaymentProcessorServiceExecutor {
     }
 
     public void fireAndForget(NewPaymentRequest newPaymentRequest) {
-        fireAndForget(newPaymentRequest, new NewPaymentItemExecutor(healthState, payments));
+        queue.offer(newPaymentRequest);
+        //fireAndForget(newPaymentRequest, new NewPaymentItemExecutor(healthState, payments));
     }
 
     private void fireAndForget(NewPaymentRequest newPaymentRequest, Consumer<NewPaymentRequest> consumer) {
