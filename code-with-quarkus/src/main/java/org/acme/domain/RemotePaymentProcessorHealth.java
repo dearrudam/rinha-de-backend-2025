a2dd9307cf0d3d524d185456ccd0d2653abf9149
Maximillian Arruda <dearrudam@gmail.com>
2025-07-19 05:58:07 -0300

    feat: add HEALTH constant to RemotePaymentProcessorHealth
    Introduced a HEALTH constant to represent a healthy state
    for the RemotePaymentProcessorHealth record, improving clarity
    and usability in health checks.
    
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- code-with-quarkus/src/main/java/org/acme/domain/RemotePaymentProcessorHealth.java
+++ code-with-quarkus/src/main/java/org/acme/domain/RemotePaymentProcessorHealth.java
@@ -3,5 +3,6 @@ package org.acme.domain;
 public record RemotePaymentProcessorHealth(boolean failing, int minResponseTime) {
 
     public static RemotePaymentProcessorHealth UNHEALTH = new RemotePaymentProcessorHealth(true, 0);
+    public static RemotePaymentProcessorHealth HEALTH = new RemotePaymentProcessorHealth(false, 0);
 
 }
