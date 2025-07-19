0f79da7dfbe791dd8672380af8e6f2ffb88e1601
Maximillian Arruda <dearrudam@gmail.com>
2025-07-19 05:59:15 -0300

    refactor: simplify NewPaymentTask structure
    Removed unnecessary health state resolver and refactored
    the NewPaymentTask to streamline the payment processing logic.
    
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- code-with-quarkus/src/main/java/org/acme/domain/NewPaymentTask.java
+++ code-with-quarkus/src/main/java/org/acme/domain/NewPaymentTask.java
@@ -1,14 +1,10 @@
 package org.acme.domain;
 
-import io.quarkus.rest.client.reactive.QuarkusRestClientBuilder;
 import jakarta.ws.rs.core.Response;
-import org.eclipse.microprofile.config.ConfigProvider;
 import org.jboss.resteasy.reactive.RestResponse;
 
-import java.net.URI;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -16,135 +12,98 @@ import java.util.function.Function;
 import static org.acme.domain.RemotePaymentName.DEFAULT;
 import static org.acme.domain.RemotePaymentName.FALLBACK;
 
-public record NewPaymentTask(Function<RemotePaymentName, RemotePaymentProcessorHealth> healthStateResolver,
-                             BiFunction<RemotePaymentName, RemotePaymentProcessorHealth, RemotePaymentProcessorExecutor> remotePaymentProcessorExecutorFunc,
+public record NewPaymentTask(Function<RemotePaymentName, RemotePaymentProcessorExecutor> remotePaymentProcessorResolver,
                              Consumer<Payment> onSuccess) {
 
-    public static WithHealthStateResolver withHealthStateResolver(
-            Function<RemotePaymentName, RemotePaymentProcessorHealth> healthStateResolver) {
-        return new WithHealthStateResolver(healthStateResolver);
+    public static WithRemotePaymentProcessorResolver withRemotePaymentProcessorExecutorResolver(
+            Function<RemotePaymentName, RemotePaymentProcessorExecutor> remotePaymentProcessorExecutorResolver) {
+        return new WithRemotePaymentProcessorResolver(remotePaymentProcessorExecutorResolver);
     }
 
-    public record WithHealthStateResolver(
-            Function<RemotePaymentName, RemotePaymentProcessorHealth> healthStateResolver) {
+    public record WithRemotePaymentProcessorResolver(
+            Function<RemotePaymentName, RemotePaymentProcessorExecutor> remotePaymentProcessorExecutorResolver) {
 
-        public WithHealthStateResolver {
-            healthStateResolver = Objects.requireNonNull(healthStateResolver, "healthStateResolver function is required");
+        public WithRemotePaymentProcessorResolver {
+            remotePaymentProcessorExecutorResolver =
+                    Objects.requireNonNull(remotePaymentProcessorExecutorResolver, "remotePaymentProcessorExecutorResolver is required");
         }
 
-        public WithHealthStateResolverAndRemotePaymentProcessorResolver withRemotePaymentProcessorExecutorResolver(
-                BiFunction<RemotePaymentName, RemotePaymentProcessorHealth, RemotePaymentProcessorExecutor> remotePaymentProcessorExecutorResolver) {
-            return new WithHealthStateResolverAndRemotePaymentProcessorResolver(healthStateResolver, remotePaymentProcessorExecutorResolver);
-        }
-    }
-
-    public record WithHealthStateResolverAndRemotePaymentProcessorResolver(
-            Function<RemotePaymentName, RemotePaymentProcessorHealth> healthStateResolver,
-            BiFunction<RemotePaymentName, RemotePaymentProcessorHealth, RemotePaymentProcessorExecutor> remotePaymentProcessorExecutorResolver) {
-
-        public WithHealthStateResolverAndRemotePaymentProcessorResolver {
-            healthStateResolver = Objects.requireNonNull(healthStateResolver, "healthStateResolver function is required");
-            remotePaymentProcessorExecutorResolver = Objects.requireNonNull(remotePaymentProcessorExecutorResolver, "remotePaymentProcessorExecutorResolver is required");
-        }
-
-        public WithHealthStateResolverAndRemotePaymentProcessorResolver withHealthStateResolver(
-                Function<RemotePaymentName, RemotePaymentProcessorHealth> healthStateResolver) {
-            return new WithHealthStateResolverAndRemotePaymentProcessorResolver(healthStateResolver, remotePaymentProcessorExecutorResolver);
-        }
-
-        public WithHealthStateResolverAndRemotePaymentProcessorResolver withRemotePaymentProcessorExecutorResolver(
-                BiFunction<RemotePaymentName, RemotePaymentProcessorHealth, RemotePaymentProcessorExecutor> remotePaymentProcessorExecutorFunc) {
-            return new WithHealthStateResolverAndRemotePaymentProcessorResolver(healthStateResolver, remotePaymentProcessorExecutorFunc);
+        public WithRemotePaymentProcessorResolver withRemotePaymentProcessorExecutorResolver(
+                Function<RemotePaymentName, RemotePaymentProcessorExecutor> remotePaymentProcessorExecutorResolver) {
+            return new WithRemotePaymentProcessorResolver(remotePaymentProcessorExecutorResolver);
         }
 
         public NewPaymentTaskBuilder withPaymentStore(Consumer<Payment> paymentStore) {
-            return new NewPaymentTaskBuilder(healthStateResolver, remotePaymentProcessorExecutorResolver, paymentStore);
+            return new NewPaymentTaskBuilder(remotePaymentProcessorExecutorResolver, paymentStore);
         }
 
     }
 
     public record NewPaymentTaskBuilder(
-            Function<RemotePaymentName, RemotePaymentProcessorHealth> healthStateResolver,
-            BiFunction<RemotePaymentName, RemotePaymentProcessorHealth, RemotePaymentProcessorExecutor> remotePaymentProcessorExecutorResolver,
+            Function<RemotePaymentName, RemotePaymentProcessorExecutor> remotePaymentProcessorExecutorResolver,
             Consumer<Payment> paymentStore) {
 
         public NewPaymentTaskBuilder {
-            healthStateResolver = Objects.requireNonNull(healthStateResolver, "healthStateResolver function is required");
             remotePaymentProcessorExecutorResolver = Objects.requireNonNull(remotePaymentProcessorExecutorResolver, "remotePaymentProcessorExecutorResolver is required");
             paymentStore = Objects.requireNonNull(paymentStore, "paymentStore function is required");
         }
 
-        public NewPaymentTaskBuilder withHealthStateResolver(Function<RemotePaymentName, RemotePaymentProcessorHealth> healthStateResolver) {
-            return new NewPaymentTaskBuilder(healthStateResolver, remotePaymentProcessorExecutorResolver, paymentStore);
-        }
-
-        public NewPaymentTaskBuilder withRemotePaymentProcessorExecutorFunc(
-                BiFunction<RemotePaymentName, RemotePaymentProcessorHealth, RemotePaymentProcessorExecutor> remotePaymentProcessorExecutorFunc) {
-            return new NewPaymentTaskBuilder(healthStateResolver, remotePaymentProcessorExecutorFunc, paymentStore);
+        public NewPaymentTaskBuilder withRemotePaymentProcessorExecutorResolver(
+                Function<RemotePaymentName, RemotePaymentProcessorExecutor> remotePaymentProcessorExecutorResolver) {
+            return new NewPaymentTaskBuilder(remotePaymentProcessorExecutorResolver, paymentStore);
         }
 
         public NewPaymentTaskBuilder withPaymentStore(Consumer<Payment> paymentStore) {
-            return new NewPaymentTaskBuilder(healthStateResolver, remotePaymentProcessorExecutorResolver, paymentStore);
+            return new NewPaymentTaskBuilder(remotePaymentProcessorExecutorResolver, paymentStore);
         }
 
         public NewPaymentTask build() {
-            return new NewPaymentTask(healthStateResolver, remotePaymentProcessorExecutorResolver, paymentStore);
+            return new NewPaymentTask(remotePaymentProcessorExecutorResolver, paymentStore);
         }
     }
 
-    public Optional<Payment> execute(RemotePaymentRequest newPayment) throws IllegalStateException, RemotePaymentProcessorNotAvailableException, PaymentProcessException {
+    public Optional<Payment> execute(RemotePaymentRequest newPayment) throws RemotePaymentProcessorNotAvailableException, PaymentProcessException {
 
-        RemotePaymentProcessorHealth defaultHealth = DEFAULT.healthState(healthStateResolver);
-        RemotePaymentProcessorHealth fallbackHealth = FALLBACK.healthState(healthStateResolver);
+        RemotePaymentProcessorExecutor defaultPaymentProcessor = remotePaymentProcessorResolver.apply(DEFAULT);
+        RemotePaymentProcessorExecutor fallbackPaymentProcessor = remotePaymentProcessorResolver.apply(FALLBACK);
 
-        RemotePaymentName remotePaymentName = DEFAULT;
-        RemotePaymentProcessorHealth actualHealth = defaultHealth;
-        if (defaultHealth.failing() && !fallbackHealth.failing()) {
-            remotePaymentName = FALLBACK;
-            actualHealth = fallbackHealth;
-        }
+        return submitPayment(newPayment,
+                DEFAULT,
+                defaultPaymentProcessor,
+                fallbackRequest ->
+                        submitPayment(fallbackRequest, FALLBACK, fallbackPaymentProcessor, rf -> Optional.empty()));
+    }
 
-        if (actualHealth.failing()) {
-            throw new RemotePaymentProcessorNotAvailableException(remotePaymentName, STR."All remote payment services are failing. Please try again later.");
-        }
 
-        var remotePaymentProcessorExecutor = remotePaymentProcessorExecutorFunc.apply(remotePaymentName, actualHealth);
-
-        RestResponse<RemotePaymentResponse> response = null;
+    private Optional<Payment> submitPayment(RemotePaymentRequest newPayment,
+                                            RemotePaymentName remotePaymentName,
+                                            RemotePaymentProcessorExecutor paymentProcessor,
+                                            Function<RemotePaymentRequest, Optional<Payment>> fallback) throws RemotePaymentProcessorNotAvailableException, PaymentProcessException {
+        RestResponse<RemotePaymentResponse> response;
         try {
-            System.out.println(STR."Sending \{newPayment} to \{remotePaymentName} remote payment service");
-            response = remotePaymentProcessorExecutor.processPayment(newPayment);
+            response = paymentProcessor.processPayment(newPayment);
         } catch (Exception e) {
-            throw new RemotePaymentProcessorNotAvailableException(remotePaymentName,
-                    STR."Unexpected error from \{remotePaymentName} remote payment service: \{e.getMessage()}. It'll be re-submitted...", e);
+            return Optional.of(fallback.apply(newPayment)
+                    .orElseThrow(() -> new RemotePaymentProcessorNotAvailableException(remotePaymentName,
+                            "Unexpected error on submit to " + remotePaymentName.value() + " remote payment service. : " + e.getMessage())));
         }
-        switch (Response.Status.fromStatusCode(response.getStatus()).getFamily()) {
+        final var _response = response;
+        switch (Response.Status.fromStatusCode(_response.getStatus()).getFamily()) {
             case SUCCESSFUL -> {
                 Payment payment = remotePaymentName.toPayment(newPayment);
                 onSuccess.accept(payment);
                 return Optional.of(payment);
             }
+            case SERVER_ERROR -> {
+                return Optional.of(fallback.apply(newPayment)
+                        .orElseThrow(() -> new RemotePaymentProcessorNotAvailableException(remotePaymentName,
+                                "Unexpected value: " + _response.getStatus() + " from " + remotePaymentName.value() + " remote payment service")));
+            }
             default -> {
-                System.out.println(STR."\{remotePaymentName} : \{response.getStatus()} - \{response.getEntity().message()}");
-                if (DEFAULT.equals(remotePaymentName)) {
-                    throw new RemotePaymentProcessorNotAvailableException(remotePaymentName,
-                            STR."Unexpected value: \{response.getStatus()} from \{remotePaymentName.value()} remote payment service. It'll be re-submitted...");
-                }
                 throw new PaymentProcessException(remotePaymentName,
-                        STR."Unexpected value: \{response.getStatus()} from \{remotePaymentName.value()} remote payment service. It'll be re-submitted...");
+                        "Unexpected value: " + response.getStatus() + " from " + remotePaymentName.value() + " remote payment service.");
             }
         }
-    }
-
-    public static RemotePaymentProcessorExecutor remotePaymentExecutorOf(RemotePaymentName remotePaymentName, RemotePaymentProcessorHealth actualHealth) {
-        URI uri = URI.create(ConfigProvider.getConfig()
-                .getValue("%s-payment-processor.url".formatted(remotePaymentName.value()), String.class));
-        long timeout = 1500;
-        return QuarkusRestClientBuilder.newBuilder()
-                .baseUri(uri)
-                .connectTimeout(timeout, TimeUnit.MILLISECONDS)
-                .readTimeout(timeout, TimeUnit.MILLISECONDS)
-                .build(RemotePaymentProcessorExecutor.class);
     }
 
 }
