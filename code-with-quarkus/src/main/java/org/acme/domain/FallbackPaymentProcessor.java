22830f42bae2d0207545b8bd2a04a3cbc98db1fd
Maximillian Arruda <dearrudam@gmail.com>
2025-07-19 05:56:51 -0300

    feat: extend payment processor interfaces
    Added RemotePaymentProcessorExecutor to DefaultPaymentProcessor
    and FallbackPaymentProcessor interfaces to enhance payment processing
    capabilities.
    
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- code-with-quarkus/src/main/java/org/acme/domain/FallbackPaymentProcessor.java
+++ code-with-quarkus/src/main/java/org/acme/domain/FallbackPaymentProcessor.java
@@ -3,5 +3,6 @@ package org.acme.domain;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @RegisterRestClient(configKey = "fallback-payment-processor")
-public interface FallbackPaymentProcessor extends RemotePaymentProcessorHealthCheck {
+public interface FallbackPaymentProcessor extends
+        RemotePaymentProcessorHealthCheck, RemotePaymentProcessorExecutor {
 }
